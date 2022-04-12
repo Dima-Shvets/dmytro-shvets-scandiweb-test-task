a7a9f794a091d8c2ad6a76571734d4cf1d6e78fd
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2022-04-12 21:24:01 +0300

    adds apollo client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { gql } from "apollo-boost";
+import { ApolloProvider } from '@apollo/react-hooks';
+import { Query } from '@apollo/react-components';
+
+import ApolloClient from 'apollo-boost';
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+});
+
+client
+  .query({
+    query: gql`
+      {
+  categories{
+    name
+    products {
+      name
+    }
+  }
+}
+    `
+  })
+  .then(result => console.log(result));
+
+const CATEGORIES = gql`
+      {
+  categories{
+    name
+    products {
+      name
+    }
+  }
+}
+    `
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -17,8 +52,25 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+        </header>
+        <Query
+    query={CATEGORIES}
+  >
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data.categories[0].products.map(({ name }) => (
+              <div key={name}>
+                <p>
+                  {name}
+                </p>
+              </div>
+            ));
+          }}
+  </Query>
+    </div>  
+    </ApolloProvider>
   );
 }
 
